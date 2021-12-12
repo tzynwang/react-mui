2c8af71363f531d84abc35541348a902a9dd33ad
tzyn <tzyn.wang@gmail.com>
2021-12-12 20:27:30 +0800

    [feat] Implement palette customization

---
+++ src/themes/Palette/index.ts
@@ -0,0 +1,14 @@
+const CustomizedPalette = {
+  palette: {
+    primary: {
+      main: '#1f86ff',
+      dark: '#0075fe'
+    },
+    secondary: {
+      main: '#e5e5e5',
+      dark: '#bdbdbd'
+    }
+  }
+}
+
+export default CustomizedPalette
