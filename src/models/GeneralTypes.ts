03ff5dfe9d8e5275ab1d09c32b8228ae78ba958e
Charlie <charlie@lawsnote.com>
2021-12-18 16:01:50 +0800

    [feat] Set GeneralTypes

---
+++ src/models/GeneralTypes.ts
@@ -0,0 +1,60 @@
+export type Gender = "male" | "female";
+
+export type Title = "Mr" | "Miss" | "Ms" | "Mrs";
+
+export interface User {
+  gender: Gender;
+  name: {
+    title: Title;
+    first: string;
+    last: string;
+  };
+  location: {
+    street: {
+      number: number;
+      name: string;
+    };
+    city: string;
+    state: string;
+    country: string;
+    postcode: number;
+    coordinates: {
+      latitude: string;
+      longitude: string;
+    };
+    timezone: {
+      offset: string;
+      description: string;
+    };
+  };
+  email: string;
+  login: {
+    uuid: string;
+    username: string;
+    password: string;
+    salt: string;
+    md5: string;
+    sha1: string;
+    sha256: string;
+  };
+  dob: {
+    date: string;
+    age: number;
+  };
+  registered: {
+    date: string;
+    age: number;
+  };
+  phone: string;
+  cell: string;
+  id: {
+    name: string;
+    value: string;
+  };
+  picture: {
+    large: string;
+    medium: string;
+    thumbnail: string;
+  };
+  nat: string;
+}
