a99f511decc77a6623811534cfa1aef95bba6196
tzyn <tzyn.wang@gmail.com>
2021-12-25 20:47:23 +0800

    [feat] components/Input search feature

--- src/components/common/Input/index.tsx
+++ src/components/common/Input/index.tsx
@@ -3,17 +3,19 @@ import React from 'react'
 import TextField from '@mui/material/TextField'
 import InputAdornment from '@mui/material/InputAdornment'
 import SearchIcon from '@mui/icons-material/Search'
-
 import Button from '@mui/material/Button'
 
-function Input() {
+import { InputProps } from './types'
+
+function Input(props: InputProps) {
+  const { handleUserInput, handleSearch, handleKeyDownEnter } = props
   return (
     <React.Fragment>
       <TextField
         id="outlined-basic"
         variant="outlined"
         label={false}
-        placeholder="請輸入狐狸"
+        placeholder="Enter nationalities"
         InputProps={{
           startAdornment: (
             <InputAdornment position="start">
@@ -22,9 +24,11 @@ function Input() {
           )
         }}
         sx={{ marginRight: '8px' }}
+        onChange={handleUserInput}
+        onKeyDown={handleKeyDownEnter}
       />
-      <Button variant="contained" disableElevation>
-        搜尋
+      <Button variant="contained" onClick={handleSearch} disableElevation>
+        search
       </Button>
     </React.Fragment>
   )
